1a82f31e0419f2ba5b4ef4f8e5a151eba1778bf2
DelpinSusaiRaj <susairajs@outlook.com>
2017-03-03 12:52:34 +0530

    add all files

---
+++ XamariniOSTimer/ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UIKit;
+
+using System.Timers;
+
+namespace XamariniOSTimer
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			Timer timer = new System.Timers.Timer();
+
+			timer.Interval = 1000;
+			timer.Enabled = true;
+			timer.Elapsed += Timer_Elapsed;
+			timer.Start();
+
+
+
+		}
+
+		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			
+			DateTime myDate = DateTime.Now;
+
+			InvokeOnMainThread(() => { lblTimer.Text = myDate.ToString("F");});
+
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
